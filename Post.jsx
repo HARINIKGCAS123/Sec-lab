437f7293bd4a2a3fd1ca614a83325c0ec44edae1
HARINIKGCAS123 <24bcs215@kgcas.com>
2026-02-18 10:31:32 +0530

    Blog.jsx

---
+++ Post.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quos laboriosam culpa, vero deleniti, laudantium cupiditate vitae minus at doloremque quo quisquam voluptatem quis ab asperiores excepturi, quod mollitia quibusdam!
+        </p>
+      
+    </div>
+  )
+}
+
+export default Post
